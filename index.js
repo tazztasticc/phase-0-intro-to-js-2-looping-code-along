fb6c55b6e98b7ec982f5ddc83b5362cd11d5efcb
Tasif <taz181995@gmail.com>
2021-05-14 08:48:28 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  } 
+
+  const name = ['Lisa','Kaitlin', 'Jan'];
+  const event = 'surprise';
+
+  function writeCards(names, events){
+    let newArr= [];
+    for(let i=0; i<names.length; i++){
+        newArr.push('Thank you,${names[i]}, for the wonderful ${event} gift!');
+        debugger;
+
+        return newArr;
+    }
+}
